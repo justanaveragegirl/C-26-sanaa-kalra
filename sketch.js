a07e56682c5c3935cba3fa547b94172a0e40b7b3
justanaveragegirl <kalrasanaa@gmail.com>
2020-06-17 16:49:11 +0530

    new box

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-
+var box6;
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -23,7 +23,7 @@ function setup(){
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
-
+    box6 = new box(900,420,50,50);
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
@@ -49,7 +49,7 @@ function draw(){
     ground.display();
     pig1.display();
     log1.display();
-
+    box6.display();
     box3.display();
     box4.display();
     pig3.display();
